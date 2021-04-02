2aa44f07c645ae5ad56efc2e79738650f3dfaedb
Blake Sticland <blakelists@yahoo.com.au>
2021-04-02 23:41:05 +1100

    added db.js file, fixed icons to be square images, checked that indexedDB was working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,30 @@
-// Dependencies
-const path = require('path');
+// // Dependencies
+// const path = require('path');
 
-module.exports = (app) => {
-    // Routes
-    // Each of the below routes just handles the HTML page that the user gets sent to.
+// module.exports = (app) => {
+//     // Routes
+//     // Each of the below routes just handles the HTML page that the user gets sent to.
     
-    // '/' route loads index.html
-    app.get('/', (req, res) =>
-        res.sendFile(path.join(__dirname, '../public/index.html'))
-    );
+//     // '/' route loads index.html
+//     app.get('/', (req, res) =>
+//         res.sendFile(path.join(__dirname, '../public/index.html'))
+//     );
     
-    // '/stats' route loads stats.html
-    app.get('/stats', (req, res) =>
-        res.sendFile(path.join(__dirname, '../public/stats.html'))
-    );
+//     // '/stats' route loads stats.html
+//     app.get('/stats', (req, res) =>
+//         res.sendFile(path.join(__dirname, '../public/stats.html'))
+//     );
     
-    // '/exercise' route loads exercise.html
-    app.get('/budget', (req, res) =>
-        res.sendFile(path.join(__dirname, '../public/budget.html'))
-    );
+//     // '/exercise' route loads exercise.html
+//     app.get('/budget', (req, res) =>
+//         res.sendFile(path.join(__dirname, '../public/budget.html'))
+//     );
+// };
+
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
-
